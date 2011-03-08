0cdb0b6dac13c110f8638ceb8b766c8911a5a33f
Lyle Underwood <lyle.underwood@bkwld.com>
2011-03-08 01:35:18 -0800

    Added breadcrumbs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,42 @@
 module ApplicationHelper
+  def breadcrumb target
+    trail = []
+    if target.is_a? Category
+      path = target.ancestors
+      path << target
+      p path
+      trail = path.collect do |category|
+        if category.name.eql? 'ROOT'
+          link_to('Home', root_path)
+        else
+          link_to(category.name, category)
+        end
+      end
+      p trail
+    elsif target.is_a? Album
+      path = target.category.ancestors
+      path << target.category
+      trail = path.collect do |category|
+        if category.name.eql? 'ROOT'
+          link_to('Home', root_path)
+        else
+          link_to(category.name, category)
+        end
+      end
+      trail << link_to(target.name, [target.category, target])
+    elsif target.is_a? Image
+      path = target.album.category.ancestors
+      path << target.album.category
+      trail = path.collect do |category|
+        if category.name.eql? 'ROOT'
+          link_to('Home', root_path)
+        else
+          link_to(category.name, category)
+        end
+      end
+      trail << link_to(target.album.name, [target.album.category, target.album])
+      trail << link_to(target.name, [target.album.category, target.album, target])
+    end
+    trail
+  end
 end
