0eb8a2005a89dbdec740873636a5ba5b72791227
Lyle Underwood <lyle.underwood@bkwld.com>
2011-03-08 22:01:10 -0800

    Added album and image counts to home categories.

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,23 @@ class Category < ActiveRecord::Base
   slug :name
 
   has_many :albums
+
+  def contained_albums
+    categories = descendants + [self]
+
+    sub_albums = []
+    categories.each do |category|
+      sub_albums = sub_albums + category.albums
+    end
+    sub_albums
+  end
+  
+  def contained_images
+    sub_images = []
+    contained_albums.each do |album|
+      sub_images = sub_images + album.images
+    end
+    sub_images
+  end
+
 end
