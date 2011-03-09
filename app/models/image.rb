64e7ce9f4b778826065794685e4f796ae9eb6369
Lyle Underwood <lyle.underwood@bkwld.com>
2011-03-09 00:14:50 -0800

    Completed almost everything on lisa's list.

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,5 @@
 class Image < ActiveRecord::Base
-  has_attached_file :subject, :styles => {:thumb => "75x75#", :view => "600x400>"}
+  has_attached_file :subject, :styles => {:thumb => "100x100>", :view => "600x400>"}
 
   belongs_to :album
 
