90df67ecbd12ecc41ac5045313af497dda391dd6
Lyle Underwood <lyle.underwood@bkwld.com>
2012-01-25 01:27:27 -0800

    add image popup

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+jQuery(function() {
+  jQuery('.detailImage').each(function(i, img) {
+    img = jQuery(img);
+    img.popupWindow({
+      width: img.attr('data-width'),
+      height: img.attr('data-height'),
+      top: 0,
+      left: Math.round((screen.width / 2) - img.attr('data-width') / 2)
+    });
+  });
+});
